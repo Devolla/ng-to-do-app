eaf291431c8a1ab4eaf7cabb9848991cb737673c
Ola Pawlus <pawlus.ola@gmail.com>
2022-01-22 18:47:35 +0100

    services

--- todo-list/src/app/list-manager/list-manager.component.ts
+++ todo-list/src/app/list-manager/list-manager.component.ts
@@ -1,34 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
+import { TodoListService } from '../services/todo-list.service';
 
 @Component({
   selector: 'app-list-manager',
   template: `
+   <div class="todo-app">
   <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
 
   <ul>
   <li *ngFor="let todoItem of todoList">
     <app-todo-item [item]="todoItem"></app-todo-item>
   </li>
-</ul>`,
+</ul>
+   </div>`,
   styleUrls: ['./list-manager.component.css']
 })
 export class ListManagerComponent implements OnInit {
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
-  constructor() { }
-
-  ngOnInit(): void {
+  todoList: TodoItem[];
+ constructor(private todoListService: TodoListService) { 
   }
 
+  ngOnInit(): void {
+    this.todoList = this.todoListService.getTodoList();
+  }
   addItem(title: string) {    
     this.todoList.push({ title });
   }
+  
 
 }
